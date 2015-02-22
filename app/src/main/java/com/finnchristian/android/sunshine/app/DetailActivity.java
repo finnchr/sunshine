8bb117d8f629bedfe184608e0e9750c1a4eb3910
Finn Christian <fcreusch@gmail.com>
2015-02-23 00:03:57 +0100

    Forgot to import the Log class.

--- app/src/main/java/com/finnchristian/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/finnchristian/android/sunshine/app/DetailActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
